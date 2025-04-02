95bd4a5c7e8bcb8886f7f1a86021b01fd20ba63a
Yusuf Ibrahim <yusuf@softwareseni.com>
2025-04-02 14:03:31 +0700

    Added basic database support for books CRUD

---
+++ src/main/java/org/acme/entity/Book.java
@@ -0,0 +1,10 @@
+package org.acme.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Book extends PanacheEntity {
+    public String title;
+    public String author;
+}
