6ae9fbd21c82258bd7c40e1b223d257069ca190b
Yusuf Ibrahim <yusuf@softwareseni.com>
2025-04-04 12:30:02 +0700

    Greeting service

--- src/main/java/org/acme/rest/GreetingResource.java
+++ src/main/java/org/acme/rest/GreetingResource.java
@@ -1,13 +1,18 @@
 package org.acme.rest;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.acme.service.GreetingService;
 import org.jboss.resteasy.reactive.RestPath;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService service;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -21,5 +26,12 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@RestPath String name) {
         return "Hello " + name +  "!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(String name) {
+        return service.greeting(name);
     }
 }
---
+++ src/main/java/org/acme/service/GreetingService.java
@@ -0,0 +1,12 @@
+package org.acme.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+
+}
